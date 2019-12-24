17506f2b8862a2a288159f3fe8d0392a434bddb9
Traineau <nathan.trn@orange.fr>
2019-12-24 11:36:48 +0100

    fixxx

--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -1,12 +1,16 @@
 const { Pool } = require('pg')
 import 'dotenv/config';
 
+const poool = new Pool({
+  connectionString: 'postgresql://plvmwubdstzdsj:07ac1a1a2e1da9c5cfb0a4e012105bcc7ef1c19ccbe7fe10aca50032795c2247@ec2-79-125-2-142.eu-west-1.compute.amazonaws.com:5432/d34vdkse2d19j4',
+})
+
 var pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    user: 'plvmwubdstzdsj',
+    password:'07ac1a1a2e1da9c5cfb0a4e012105bcc7ef1c19ccbe7fe10aca50032795c2247',
+    database: 'd34vdkse2d19j4',
     port: 5432,
-    host: process.env.DB_HOST,
+    host: 'ec2-79-125-2-142.eu-west-1.compute.amazonaws.com',
     ssl: true
 }); 
 
